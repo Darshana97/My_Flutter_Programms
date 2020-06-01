faa499eeaec01435c11947ceaf0b0f73a000f81e
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-01 13:08:11 +0530

    Expanded Widget

---
+++ expanded_widget/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expanded Widget"),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              child: Text("1"),
+              color: Colors.pinkAccent,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              child: Text("2"),
+              color: Colors.amberAccent,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(30.0),
+              child: Text("3"),
+              color: Colors.greenAccent,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
