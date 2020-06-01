fb764b4ffdea3156036f5805d687a05ded1d97fa
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-01 11:29:53 +0530

    Image and Assets

---
+++ image_and_assets/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image and Assets"),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+      ),
+//      body:Image(image: NetworkImage("url")),
+      body: Center(
+        child: Image(image: AssetImage("assets/p2.jpg")),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("Click"),
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
