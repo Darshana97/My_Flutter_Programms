2769040f7dbe90169ecbbbfd10d3af80cca3646a
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-02 09:14:43 +0530

    Id UI

---
+++ id_ui/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Ui(),
+));
+
+class Ui extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("My Id Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+    );
+  }
+}
+
+
