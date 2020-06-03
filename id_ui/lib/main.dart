62b74c5d77243c85ab30ae4658ed08a12fa54014
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-03 21:37:19 +0530

    Id UI Interface

--- id_ui/lib/main.dart
+++ id_ui/lib/main.dart
@@ -18,9 +18,62 @@ class Ui extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/p1.JPG"),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
             Text("NAME",
-                style: TextStyle(color: Colors.grey, letterSpacing: 2.0))
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("Darshana Senevirathna",
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text("CURRENT SOFTWARE ENGINEERING LEVEL",
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("2",
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "darshana@gmail.com",
+                  style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0),
+                )
+              ],
+            )
           ],
         ),
       ),
