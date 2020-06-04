b55d21b70e3c44e6b674cfdc1a1a320197dfd309
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-06-04 18:45:25 +0530

    Extracting Widget

---
+++ extracting_widget/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+      home: ExtractingWidget(),
+    ));
+
+class ExtractingWidget extends StatefulWidget {
+  @override
+  _ExtractingWidgetState createState() => _ExtractingWidgetState();
+}
+
+class _ExtractingWidgetState extends State<ExtractingWidget> {
+
+  List<Quote> quotes = [
+    Quote(text: "hello world",author: "Darshana"),
+    Quote(text: "software engineering",author: "pubudu"),
+  ];
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Extracting Widget"),
+        centerTitle: true,
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote)=>QuoteCard(quote:quote)).toList(),
+      ),
+    );
+  }
+}
+
+
